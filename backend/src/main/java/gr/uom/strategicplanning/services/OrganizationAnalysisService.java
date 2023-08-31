5a635f6a82063a1bc4dde53c2313db41207f1a63
ArchontisKostis <arxontisk02@gmail.com>
2023-08-31 17:11:19 +0300

    Fix bugs on total languages calculation

--- backend/src/main/java/gr/uom/strategicplanning/services/OrganizationAnalysisService.java
+++ backend/src/main/java/gr/uom/strategicplanning/services/OrganizationAnalysisService.java
@@ -47,10 +47,8 @@ public class OrganizationAnalysisService {
         Collection<Project> projects = organization.getProjects();
 
         // Now parse all the languages and make organization languages with the total loc
-        for (Project project : projects) {
-            languageService.updateOrganizationLanguages(organization);
-        }
-
+        languageService.updateOrganizationLanguages(organization);
+        saveOrganizationAnalysis(organizationAnalysis);
 
         getGeneralStats(organization);
         organizationAnalysis.setActivityStats(getActivityStats(organization));
